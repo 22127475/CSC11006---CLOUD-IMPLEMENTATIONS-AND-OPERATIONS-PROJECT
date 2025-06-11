478e08898a3dc3bc0e8c674e7abeaaa1d172d3db
Diep Gia Huy <dghuy121104@gmail.com>
2025-06-11 19:33:33 +0700

    auto

--- project01/Web/ProductService/server.js
+++ project01/Web/ProductService/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const productRoutes = require('./routes/index');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const productRoutes = require("./routes/index");
 
 const app = express();
 const port = process.env.PORT || 3002;
-const hostURL = process.env.HOST_URL || 'http://localhost:8080';
+const hostURL = process.env.HOST_URL || "http://localhost:8080";
 
 app.use(cors({ origin: hostURL, credentials: true }));
 app.use(express.json());
 
-app.use('/products', productRoutes);
+app.use("/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Product service đang chạy tại http://localhost:${port}`);
