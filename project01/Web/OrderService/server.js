4ea825599da164ce95c9bc1988938977f8ee8666
Diep Gia Huy <81817335+22127475@users.noreply.github.com>
2025-06-23 14:09:38 +0700

    Update server.js

--- project01/Web/OrderService/server.js
+++ project01/Web/OrderService/server.js
@@ -8,7 +8,7 @@ const orderRoutes = require('./routes/index');
 const app = express();
 const port = process.env.PORT || 3003;
 const hostURL = process.env.HOST_URL || 'http://localhost:8080';
-const ALB_DNS = process.env.ALB_DNS  + ":8080"|| 'http://localhost:8080';
+const ALB_DNS = "http://" + process.env.ALB_DNS  + ":8080"|| 'http://localhost:8080';
 const corsOptions = {
   origin: [hostURL, ALB_DNS],
   credentials: true
