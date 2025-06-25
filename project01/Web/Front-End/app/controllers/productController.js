7e688864d6b481d81c1d7f2d24e20573e7576ff3
Diep Gia Huy <81817335+22127475@users.noreply.github.com>
2025-06-25 22:28:37 +0700

    Config to show IP

--- project01/Web/Front-End/app/controllers/productController.js
+++ project01/Web/Front-End/app/controllers/productController.js
@@ -5,3 +5,11 @@ exports.index = (req, res) => {
 exports.getAllProducts = (req, res) => {
        res.render('layout', { content: 'product', apiBaseUrl: process.env.API_BASE_URL  || 3002 });
 };
+
+exports.index = (req, res) => {
+  res.render('layout', {
+    content: 'home',
+    apiBaseUrl: process.env.API_BASE_URL,
+    ec2IP: process.env.EC2_IP
+  });
+};
